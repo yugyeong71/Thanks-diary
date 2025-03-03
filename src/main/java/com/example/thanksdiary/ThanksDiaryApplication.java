f6728275272e333ef565b8dc90aba4b1ca6bb830
yugyeong71 <youkyoung42@gmail.com>
2025-03-03 19:06:16 +0900

    first commit

---
+++ src/main/java/com/example/thanksdiary/ThanksDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.thanksdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThanksDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThanksDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thanksdiary/ThanksDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thanksdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThanksDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
