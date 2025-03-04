3bca06bcbffb017e095d933a3dd6fbf6d7a53196
yugyeong71 <youkyoung42@gmail.com>
2025-03-04 20:46:17 +0900

    feat: UserEntity 구현

---
+++ src/main/java/com/example/thanksdiary/domain/user/User.java
@@ -0,0 +1,36 @@
+package com.example.thanksdiary.domain.user;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.SQLRestriction;
+
+import com.example.thanksdiary.domain.base.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter @Builder
+@AllArgsConstructor @NoArgsConstructor
+@SQLRestriction("deletedAt IS NULL")
+@Table(name = "user")
+public class User extends BaseEntity {
+
+	@Column(length = 100, nullable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = true)
+	private String refreshToken;
+
+	@Column(nullable = true)
+	private LocalDateTime lastLoginDate;
+
+}
